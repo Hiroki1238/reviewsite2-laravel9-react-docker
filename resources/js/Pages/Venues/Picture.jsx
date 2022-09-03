2fdf0b4ca3aba833d9947e16543c6320e6956e76
Hiroki1238 <c14h14n3nao3s@gmail.com>
2022-09-03 12:43:25 +0900

    [modified]jetstreamを削除

--- resources/js/Pages/Venues/Picture.jsx
+++ resources/js/Pages/Venues/Picture.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Authenticated from "@/Layouts/Authenticated";
 import ImageList from '@mui/material/ImageList';
 import { ImageListItem } from "@mui/material";
+import Guest from "@/Layouts/Guest";
 
 //レビュー一覧で選択したレビューの詳細画面
 const Picture = (props) => {
@@ -9,9 +10,9 @@ const Picture = (props) => {
     console.log(images);
 
     return (
-      <Authenticated
-      auth={props.auth}
-      >
+        <div>
+            {auth.user != null ? (
+                <Authenticated auth={props.auth}>
       
         <div>
             <ImageList className="w-full h-full"
@@ -42,7 +43,40 @@ const Picture = (props) => {
       ))} */}
         </div>
         </Authenticated>
-    );
+    ) : (
+        <Guest>
+            <div>
+            <ImageList className="w-full h-full"
+                // sx={{ width: 1000, height: 1000 }}
+                cols={3}
+                // rowHeight={164}
+            >
+                {images.map((item) => (
+                    <ImageListItem key={item.id}>
+                        <img className="aspect-square rounded-sm"
+                            src={`${item.image_path}?w=164&h=164&fit=crop&auto=format`}
+                            srcSet={`${item.image_path}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                            // alt={item.title}
+                            loading="lazy"
+                        />
+                    </ImageListItem>
+                ))}
+            </ImageList>
+
+            {/* <img src={`${images[0].image_path}`}></img>
+      <div>
+      {images.map((image) => (
+      <div key={image.id}>
+      <h2 className="m-2">
+      <img src={`${image.image_path}`}/>
+      </h2>
+      </div>
+      ))} */}
+        </div>
+        </Guest>
+    )}
+        </div>
+    )
 };
 
 export default Picture;
--- resources/js/Pages/Venues/Show.jsx
+++ resources/js/Pages/Venues/Show.jsx
@@ -5,6 +5,7 @@ import { Link, useForm } from "@inertiajs/inertia-react";
 import StarIcon from '@mui/icons-material/Star';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
 import ReviewList from "@/Components/ReviewList";
+import Guest from "@/Layouts/Guest";
 //import SweetAlert2 from 'react-sweetalert2';
 
 // このファイルは会場の情報とこの会場についているレビュー一覧を表示
@@ -35,8 +36,10 @@ const Index = (props) => {
     };
 
     return (
-        <Authenticated
-            auth={props.auth}>
+        
+        <div>
+        {auth.user != null ? (
+            <Authenticated auth={props.auth}>
 
             <div className="p-6 mt-9 shadow-lg shadow-shadowgray1 bg-gray-200 my-0 mx-auto rounded-lg border border-gray-300 text-center w-5/7">
                 <h1 className="text-title-purple1 text-3xl mt-4 mb-7">{venue.name}のレビュー</h1>
@@ -77,7 +80,50 @@ const Index = (props) => {
               <ReviewList reviews={reviews} />
             </div>
         </Authenticated>
-    );
+    ) : (
+        <Guest>
+            <div className="p-6 mt-9 shadow-lg shadow-shadowgray1 bg-gray-200 my-0 mx-auto rounded-lg border border-gray-300 text-center w-5/7">
+                <h1 className="text-title-purple1 text-3xl mt-4 mb-7">{venue.name}のレビュー</h1>
+
+              <div>
+                <h2 className="py-1">収容人数 : {venue.scale_standing}人</h2>
+                <h2 className="py-1">所在地 : {venue.address}</h2>
+                <h2 className="py-1">
+                    ホームページ : <a className="text-gray-500"  target="_blank" href={venue.url}>{venue.url}</a>
+                </h2>
+                
+                 <br/>
+                 <div className="mb-3">
+                <Link className="text-gray-500 text-xl no-underline" href={`/prefectures/venues/pictures/${venue.id}`}>[ {venue.name}の画像一覧 ]</Link>
+                </div>
+
+                <h2>
+                    <Link className="text-my-purple3 no-underline" href={`/reviews/${venue.id}/create`}>[ 新規投稿 ]</Link>
+                    
+                </h2>
+                </div>
+                <div className="mt-3 border border-b-0 border-gray-300">
+                </div>
+
+                <div className="w-1/2 ml-auto text-right space-x-3 mt-3" >
+                <Link className="text-gray-400 hover:text-gray-600 no-underline text-lg" onClick={() => window.history.back()}>
+                        戻る
+                    </Link>
+{/* 
+                {isLiked ? (<button onClick={handleUnlike}> <StarIcon className="text-yellow-500"/> </button>)
+                : (<button onClick={handleLike}> <StarBorderIcon className="text-yellow-500"/> </button>)
+              } */}
+              </div>
+
+            </div>
+
+            <div className="p-6 mt-7 shadow-lg shadow-shadowgray1 bg-gray-200 my-0 mx-auto rounded-lg border border-gray-300 text-center w-5/7">
+              <ReviewList reviews={reviews} />
+            </div>
+        </Guest>
+    )}
+    </div>
+    )
 };
 
 export default Index;
