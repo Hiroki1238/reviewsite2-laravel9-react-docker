742d48e3e98467df44b512d3b29de17df27a4b4f
Hiroki1238 <c14h14n3nao3s@gmail.com>
2022-09-23 14:44:30 +0900

    [modified]ロゴの表示の不具合の修正

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function ApplicationLogo({ className }) {
     return (
-        <img className={className} src={"logo/logo-transparent.png"} />
+        <img className={className} src={"/logo/logo-transparent.png"} />
     );
 }
